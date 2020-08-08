5906de92338767adda00cb8531e6e7e99fd14bc1
unknown <aastha.rawat33@gmail.com>
2020-08-08 16:22:05 +0530

    Awesome Changes!

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,11 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 const imagemin = require("gulp-imagemin");
 
-(browserSync = require("browser-sync").create()),
-  browserSync.init({
-    server: ".",
-  });
-
-gulp.task("sass", function () {
+gulp.task("sass-build", function () {
   return gulp
     .src("./sass/**/*.scss")
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./css"))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest("./css"));
 });
 
-gulp.task("run", gulp.series(["sass"]));
-
-gulp.watch("./sass", gulp.series("sass"), browserSync.reload);
-gulp.watch("./index.html").on("change", browserSync.reload);
-// gulp.watch("/js/modal.js", gulp.series("js"), browserSync.reload);
-
-gulp.task("watch", async function () {
-  gulp.watch("./sass/*.sass", gulp.series("sass"));
-});
-
-gulp.task("default", gulp.series(["run", "watch"]));
+gulp.task("build", gulp.series(["sass-build"]));
