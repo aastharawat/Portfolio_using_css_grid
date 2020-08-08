52efd4cc611eaef8191eb8a6c8345014990ec5e2
unknown <aastha.rawat33@gmail.com>
2020-08-08 16:00:04 +0530

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,28 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var uglifycss = require('gulp-uglifycss');
-browserSync = require('browser-sync').create(),
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+const imagemin = require("gulp-imagemin");
 
-browserSync.init({
-    server: '.'
-    });
-
-gulp.task('sass', async function () {
-    return gulp.src('./sass/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./css'));
-  }); 
-
- 
-  gulp.task('css', async function () {
-    gulp.src('./css/*.css')
-      .pipe(uglifycss({
-        "uglyComments": true
-      }))
-      .pipe(gulp.dest('./dist/'));
+(browserSync = require("browser-sync").create()),
+  browserSync.init({
+    server: ".",
   });
 
-  gulp.task('run', gulp.series( ['sass', 'css']));
+gulp.task("sass", function () {
+  return gulp
+    .src("./sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
+});
 
-    gulp.watch('./scss/**/*.scss', gulp.series('sass'), browserSync.reload);
-    gulp.watch('./*.html').on('change', browserSync.reload);
-    gulp.watch('./js/**/*.js', browserSync.reload);
-  gulp.task('watch', async function(){
-      gulp.watch('./sass/*.sass', gulp.series('sass'));
-      gulp.watch('./css/*.css', gulp.series('css'))
-  });
- 
+gulp.task("run", gulp.series(["sass"]));
 
-gulp.task('default', gulp.series(['run', 'watch']));
+gulp.watch("./sass", gulp.series("sass"), browserSync.reload);
+gulp.watch("./index.html").on("change", browserSync.reload);
+// gulp.watch("/js/modal.js", gulp.series("js"), browserSync.reload);
+
+gulp.task("watch", async function () {
+  gulp.watch("./sass/*.sass", gulp.series("sass"));
+});
+
+gulp.task("default", gulp.series(["run", "watch"]));
